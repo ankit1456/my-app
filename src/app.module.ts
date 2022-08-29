2cf767d28284ff2cf26b48ef4a2c798d50e6ee20
ankittripathi <ankittripathi1456@gmail.com>
2022-08-29 11:02:41 +0530

    refactor:fixed import errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,12 @@ import { AppController } from './app.controller';
 import 'dotenv/config';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { SharedModule } from './shared/shared.module';
-import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { BrandModule } from './brand/brand.module';
 import { ProductsModule } from './products/products.module';
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URL),
-    SharedModule,
-    AuthModule,
     CategoryModule,
     BrandModule,
     ProductsModule,
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { createdocument } from './swagger/swagger';
-import { LoggingInterceptor } from './shared/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,8 +9,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, createdocument(app));
   const globalPrefix = 'api/v1';
   app.setGlobalPrefix(globalPrefix);
-
-  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(3000);
 }
 bootstrap();
